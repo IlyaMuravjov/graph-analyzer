f6f1796339978f6705f919c63f26412287bd8e49
IlyaMuravjov <muravjovilya@gmail.com>
2021-05-10 04:58:17 +0300

    Add OpenSQLiteAnalyzedGraph integration test

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,11 @@ val `exposed-version`: String by project
 val `sqlite-jdbc-version`: String by project
 val `slf4j-version`: String by project
 
+val `junit-jupiter-version`: String by project
+val `testfx-version`: String by project
+val `monocle-version`: String by project
+val `mockk-version`: String by project
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
@@ -26,6 +31,14 @@ dependencies {
     implementation("org.xerial:sqlite-jdbc:$`sqlite-jdbc-version`")
     implementation("org.slf4j:slf4j-api:$`slf4j-version`")
     implementation("org.slf4j:slf4j-simple:$`slf4j-version`")
+
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$`junit-jupiter-version`")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$`junit-jupiter-version`")
+    testImplementation("org.testfx:testfx-core:$`testfx-version`")
+    testImplementation("org.testfx:testfx-junit5:$`testfx-version`")
+    testImplementation("org.testfx:openjfx-monocle:$`monocle-version`")
+    testImplementation("io.mockk:mockk:$`mockk-version`")
 }
 
 application {
@@ -34,4 +47,14 @@ application {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    if (project.hasProperty("headless") && project.property("headless").toString().toBoolean()) {
+        systemProperty("testfx.robot", "glass")
+        systemProperty("testfx.headless", "true")
+        systemProperty("prism.order", "sw")
+        systemProperty("prism.text", "t2k")
+    }
 }
